ffc505f4c49f02b3515068397e9fcbfe2645846b
deokjinchoe <59638057+deokjinchoe@users.noreply.github.com>
2020-08-09 05:58:01 +0900

    JS: Navbar menu click

--- main.js
+++ main.js
@@ -5,8 +5,22 @@ const navber = document.querySelector('#navbar');
 const navberHeight = navber.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
   if (window.scrollY > navberHeight) {
-    navber.cla  ssList.add('navber--dark');
+    navber.classList.add('navber--dark');
   } else {
     navber.classList.remove('navber--dark');
   }
+});
+
+// Handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {
+  const target = event.target;
+  const link = target.dataset.link;
+  if (link == null) {
+    return;
+  }
+
+  console.log(event.target.dataset.link);
+  const scrollTo = document.querySelector(link);
+  scrollTo.scrollIntoView({ behavior: 'smooth' });
 });
